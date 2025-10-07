efb94d446d0a05e695ea72a330821e6ccf85ab2e
Manu <jlopez221104@gmail.com>
2025-10-07 13:18:11 -0600

    News atributes updated

--- src/main/java/ScoreSense/App/model/News.java
+++ src/main/java/ScoreSense/App/model/News.java
@@ -16,6 +16,10 @@ public class News {
 
     private LocalDateTime publishDate = LocalDateTime.now();
 
+    private String author;
+    private String source_url;
+    private String image_url;
+
     @ManyToOne
     @JoinColumn(name = "team_id")
     private Team team;
@@ -23,9 +27,12 @@ public class News {
 
     public News() {}
 
-    public News(String title, String content, Team team) {
+    public News(String title, String content, String author, String source_url, String image_url, Team team) {
         this.title = title;
         this.content = content;
+        this.author = author;
+        this.source_url = source_url;
+        this.image_url = image_url;
         this.team = team;
     }
 
@@ -41,6 +48,15 @@ public class News {
 
     public LocalDateTime getPublishDate() { return publishDate; }
     public void setPublishDate(LocalDateTime publishDate) { this.publishDate = publishDate; }
+
+    public String getAuthor() { return author; }
+    public void setAuthor(String author) { this.author = author; }
+
+    public String getSource_url() { return source_url; }
+    public void setSource_url(String source_url) { this.source_url = source_url; }
+
+    public String getImage_url() { return image_url; }
+    public void setImage_url(String image_url) { this.image_url = image_url; }
 
     public Team getTeam() { return team; }
     public void setTeam(Team team) { this.team = team; }
