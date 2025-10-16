abc9f11a6c0b9b0847012ab37481eb61d1cd486d
Manu <jlopez221104@gmail.com>
2025-10-16 12:37:03 -0600

    New model attributes

--- src/main/java/ScoreSense/App/model/Coach.java
+++ src/main/java/ScoreSense/App/model/Coach.java
@@ -1,45 +1,33 @@
 package ScoreSense.App.model;
 
-import jakarta.persistence.*;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 @Entity
 @Table(name = "coaches")
 
 public class Coach {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "coach_id")
     private Long coachId;
 
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "nationality")
     private String nationality;
+
+    @Column(name = "experienced_years")
     private Integer experiencedYears;
 
     @ManyToOne
     @JoinColumn(name = "team_id")
     private Team team;
 
-    public Coach() {}
-
-    public Coach(String name, String nationality, Integer experiencedYears, Team team) {
-        this.name = name;
-        this.nationality = nationality;
-        this.experiencedYears = experiencedYears;
-        this.team = team;
-    }
-
-
-    public Long getCoachId() { return coachId; }
-    public void setCoachId(Long coachId) { this.coachId = coachId; }
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    public String getNationality() { return nationality; }
-    public void setNationality(String nationality) { this.nationality = nationality; }
-
-    public Integer getExperiencedYears() { return experiencedYears; }
-    public void setExperiencedYears(Integer experiencedYears) { this.experiencedYears = experiencedYears; }
-
-    public Team getTeam() { return team; }
-    public void setTeam(Team team) { this.team = team; }
 }
--- src/main/java/ScoreSense/App/model/Fan.java
+++ src/main/java/ScoreSense/App/model/Fan.java
@@ -1,49 +1,35 @@
 package ScoreSense.App.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "fans")
 public class Fan {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "fan_id")
     private Long fanId;
 
+    @Column(name = "username")
     private String username;
+
+    @Column(name = "social_media")
     private String socialMedia;
+
+    @Column(name = "profile_picture_url")
     private String profilePictureUrl;
+
+    @Column(name = "registered_at")
     private LocalDateTime registeredAt = LocalDateTime.now();
 
     @ManyToOne
     @JoinColumn(name = "team_id")
     private Team team;
 
-    public Fan() {}
 
-    public Fan(String username, String socialMedia, String profilePictureUrl, Team team) {
-        this.username = username;
-        this.socialMedia = socialMedia;
-        this.profilePictureUrl = profilePictureUrl;
-        this.team = team;
-    }
-
-
-    public Long getFanId() { return fanId; }
-    public void setFanId(Long fanId) { this.fanId = fanId; }
-
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
-
-    public String getSocialMedia() { return socialMedia; }
-    public void setSocialMedia(String socialMedia) { this.socialMedia = socialMedia; }
-
-    public String getProfilePictureUrl() { return profilePictureUrl; }
-    public void setProfilePictureUrl(String profilePictureUrl) { this.profilePictureUrl = profilePictureUrl; }
-
-    public LocalDateTime getRegisteredAt() { return registeredAt; }
-    public void setRegisteredAt(LocalDateTime registeredAt) { this.registeredAt = registeredAt; }
-
-    public Team getTeam() { return team; }
-    public void setTeam(Team team) { this.team = team; }
 }
