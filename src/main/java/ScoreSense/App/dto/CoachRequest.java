be92fd86879861b28935549e7cce8ed6e80daaab
Manu <jlopez221104@gmail.com>
2025-10-16 13:20:20 -0600

    fan and coach reponse created

--- src/main/java/ScoreSense/App/dto/CoachRequest.java
+++ src/main/java/ScoreSense/App/dto/CoachRequest.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+
 @Data
 public class CoachRequest {
 
---
+++ src/main/java/ScoreSense/App/dto/CoachResponse.java
@@ -0,0 +1,25 @@
+package ScoreSense.App.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CoachResponse {
+    @JsonProperty("coach_id")
+    Long coachId;
+    @JsonProperty("name")
+    String name;
+    @JsonProperty("nationality")
+    String nationality;
+    @JsonProperty("experienced_years")
+    Integer experiencedYears;
+    @JsonProperty("team_id")
+    Long teamId;
+}
