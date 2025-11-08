78a4ec0fa68656be4778b57ee3101e37b3f30122
Manu <jlopez221104@gmail.com>
2025-11-08 11:08:57 -0600

    depured

---
+++ src/main/java/scoresense/app/exception/ErrorResponse.java
@@ -0,0 +1,20 @@
+package scoresense.app.exception;
+
+import java.time.LocalDateTime;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ErrorResponse {
+    private int status;
+    private String message;
+    private LocalDateTime timestamp;
+
+    public ErrorResponse(int status, String message, LocalDateTime timestamp) {
+        this.status = status;
+        this.message = message;
+        this.timestamp = timestamp;
+    }
+}
