d2371e1b18902b12ab3d65a68c771271a7850b43
Manu <jlopez221104@gmail.com>
2025-11-21 12:50:42 -0600

    Microservices por coaches is now isolated

--- src/main/java/scoresense/app/repository/NewsRepository.java
+++
@@ -1,9 +0,0 @@
-package scoresense.app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import scoresense.app.model.News;
-
-public interface NewsRepository extends JpaRepository<News, Long> {
-    
-}
--- src/main/java/scoresense/app/repository/RefereeRepository.java
+++
@@ -1,6 +0,0 @@
-package scoresense.app.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import scoresense.app.model.Referee;
-
-public interface RefereeRepository extends JpaRepository<Referee,Long> {
-}
