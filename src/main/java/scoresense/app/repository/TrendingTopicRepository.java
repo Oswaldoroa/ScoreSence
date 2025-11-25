5a27c8ae0ea0461d26680317b7196060a2d776b0
Manu <jlopez221104@gmail.com>
2025-11-25 13:56:12 -0600

    Removed all files in apibranch

--- src/main/java/scoresense/app/repository/TrendingTopicRepository.java
+++
@@ -1,9 +0,0 @@
-package scoresense.app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import scoresense.app.model.TrendingTopic;
-
-public interface TrendingTopicRepository extends JpaRepository<TrendingTopic, Long> {
-    
-}
